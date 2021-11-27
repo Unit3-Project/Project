742fecd75e0cf80ca6c65cbc0fc34d89e89e4759
Nasser-96 <nasserabunasir96@outlook.com>
2021-11-27 13:21:41 +0300

    User added

---
+++ cinema/src/main/java/com/example/cinema/User/UserService.java
@@ -0,0 +1,42 @@
+package com.example.cinema.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {this.userRepository = userRepository;}
+
+    public List <User> getUsers (){return userRepository.findAll();}
+
+    public User getUser(String id){
+        Long user_id = Long.parseLong(id);
+        return userRepository.findById(user_id).orElse(null);
+    }
+
+    public User createUser(User user){return userRepository.save(user);}
+
+    public void deleteUser(String id){
+        Long user_id = Long.parseLong(id);
+        userRepository.deleteById(user_id);
+    }
+
+    public void updateUser(String id, User data){
+        Long user_id = Long.parseLong(id);
+        User user = userRepository.findById(user_id).orElse(null);
+
+        if (user != null){
+            user.setName(data.getName());
+            user.setEmail(data.getEmail());
+            user.setPassword(data.getPassword());
+            user.setRole(data.getRole());
+            user.setAge(data.getAge());
+            userRepository.save(user);
+        }
+    }
+}
