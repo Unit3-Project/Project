cc74771b44fff26380cde02565e6a27388f960a7
S-MW <72529306+S-MW@users.noreply.github.com>
2021-11-27 14:40:06 +0300

    Add Room controller, rep, and service

---
+++ cinema/src/main/java/com/example/cinema/Room/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.example.cinema.Room;
+
+import com.example.cinema.User.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room,Long> {
+}
