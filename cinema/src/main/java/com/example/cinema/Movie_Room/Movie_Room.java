d13613f74ebe5eab72b195a3a4e43b479351ed74
Nasser-96 <nasserabunasir96@outlook.com>
2021-12-02 16:38:50 +0300

    add Relastion between movie and room

---
+++ cinema/src/main/java/com/example/cinema/Movie_Room/Movie_Room.java
@@ -0,0 +1,84 @@
+package com.example.cinema.Movie_Room;
+
+import com.example.cinema.Movie.Movie;
+import com.example.cinema.Room.Room;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "movie_room")
+public class Movie_Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "date")
+    private Date date;
+
+    @Column(name = "price")
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    @JsonProperty(access = JsonProperty.Access.READ_WRITE)
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id")
+    @JsonProperty(access = JsonProperty.Access.READ_WRITE)
+    private Room room;
+
+    public Movie_Room() {
+    }
+
+    public Movie_Room(Long id, Date date, Double price, Movie movie, Room room) {
+        this.id = id;
+        this.date = date;
+        this.price = price;
+        this.movie = movie;
+        this.room = room;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+}
---
+++ cinema/src/main/java/com/example/cinema/Movie_Room/Movie_Room_Service.java
@@ -0,0 +1,50 @@
+package com.example.cinema.Movie_Room;
+
+import com.example.cinema.Movie.Movie;
+import com.example.cinema.Movie.MovieRepository;
+import com.example.cinema.Room.Room;
+import com.example.cinema.Room.RoomRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class Movie_Room_Service {
+
+    @Autowired
+    private  MovieRepository movieRepository;
+    @Autowired
+    private  RoomRepository roomRepository;
+    @Autowired
+    private Movie_Room_Repo movie_room_repo;
+
+//    public Movie_Room_Service(Movie_Room_Repo movie_room_repo) {
+//        this.movie_room_repo = movie_room_repo;
+//    }
+
+    public List<Movie_Room> getAllAvailableMovies()
+    {
+        return movie_room_repo.findAll();
+    }
+
+    public ResponseEntity<String> saveAvailableMovie(Movie_Room movie_room)
+    {
+        Long movie_id = movie_room.getMovie().getId();
+        Movie movie = movieRepository.getById(movie_id);
+
+        Long room_id = movie_room.getRoom().getId();
+        Room room = roomRepository.getById(room_id);
+        if (movie != null && room != null){
+            movie_room.setMovie(movie);
+            movie_room.setRoom(room);
+            movie_room_repo.save(movie_room);
+            return ResponseEntity.ok().body("Saved Successfully");
+        }
+        else {
+            return ResponseEntity.badRequest().body("Not Found???????");
+        }
+
+    }
+}
