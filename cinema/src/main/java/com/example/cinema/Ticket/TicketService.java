1d7becb9631e55b71d262b3ced76c1e6e5b5e563
Nasser-96 <nasserabunasir96@outlook.com>
2021-12-04 14:38:54 +0300

    Enhance Ticket Relation

--- cinema/src/main/java/com/example/cinema/Ticket/TicketService.java
+++ cinema/src/main/java/com/example/cinema/Ticket/TicketService.java
@@ -2,6 +2,8 @@ package com.example.cinema.Ticket;
 
 import com.example.cinema.Movie.Movie;
 import com.example.cinema.Movie.MovieRepository;
+import com.example.cinema.Movie_Room.Movie_Room;
+import com.example.cinema.Movie_Room.Movie_Room_Repo;
 import com.example.cinema.Room.Room;
 import com.example.cinema.Room.RoomRepository;
 import com.example.cinema.User.User;
@@ -18,10 +20,13 @@ public class TicketService {
 
     @Autowired
     private UserRepository userRepository;
+//    @Autowired
+//    private MovieRepository movieRepository;
+//    @Autowired
+//    private RoomRepository roomRepository;
+
     @Autowired
-    private MovieRepository movieRepository;
-    @Autowired
-    private RoomRepository roomRepository;
+    private Movie_Room_Repo movie_room_repo;
     @Autowired
     private TicketRepo ticketRepo;
 
@@ -32,19 +37,16 @@ public class TicketService {
 
     public ResponseEntity<String> saveTicket(Ticket ticket)
     {
-        Long movie_id = ticket.getMovie().getId();
-        Movie movie = movieRepository.getById(movie_id);
 
-        Long room_id = ticket.getRoom().getId();
-        Room room = roomRepository.getById(room_id);
+        Long movie_room_id = ticket.getMovie_room().getId();
+        Movie_Room movie_room = movie_room_repo.getById(movie_room_id);
 
         Long user_id = ticket.getUser().getId();
         User user = userRepository.getById(user_id);
 
 
-        if (movie != null && room != null && user != null){
-            ticket.setMovie(movie);
-            ticket.setRoom(room);
+        if ( movie_room!= null && user != null){
+            ticket.setMovie_room(movie_room);
             ticket.setUser(user);
             ticketRepo.save(ticket);
             return ResponseEntity.ok().body("Saved Successfully");
