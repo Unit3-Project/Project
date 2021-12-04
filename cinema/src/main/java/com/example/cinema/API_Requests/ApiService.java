b82841d16bf346d44a26760577e2d7fff226dfd4
S-MW <72529306+S-MW@users.noreply.github.com>
2021-12-04 18:35:04 +0300

    add API controller

---
+++ cinema/src/main/java/com/example/cinema/API_Requests/ApiService.java
@@ -0,0 +1,39 @@
+package com.example.cinema.API_Requests;
+
+import com.example.cinema.Movie.Movie;
+import com.example.cinema.Movie.MovieRepository;
+import com.example.cinema.Movie_Room.Movie_Room_Repo;
+import com.example.cinema.Room.Room;
+import com.example.cinema.Room.RoomRepository;
+import com.example.cinema.Ticket.TicketRepo;
+import com.example.cinema.User.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApiService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private MovieRepository movieRepository;
+    @Autowired
+    private RoomRepository roomRepository;
+    @Autowired
+    private Movie_Room_Repo movie_room_repo;
+    @Autowired
+    private TicketRepo ticketRepo;
+
+
+    public AdminPageData  getAdminPageData()
+    {
+        int user = userRepository.findAll().size();
+        int movie = movieRepository.findAll().size();
+        int room = roomRepository.findAll().size();
+        int availableMovie = movie_room_repo.findAll().size();
+        int ticket = ticketRepo.findAll().size();
+        AdminPageData d1 = new AdminPageData(user,movie,room,availableMovie,ticket);
+        return d1;
+    }
+
+}
